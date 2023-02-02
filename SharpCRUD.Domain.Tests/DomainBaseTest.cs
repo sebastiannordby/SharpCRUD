7400e4cdacc7587a73f5157d6ed3278f7de32093
Sebastian <sebastian.nordby.bjornstad@wlcom.no>
2023-02-02 20:16:11 +0100

    🟢SaveCustomerServiceTests: Tests that saving null customer should throw error

---
+++ SharpCRUD.Domain.Tests/DomainBaseTest.cs
@@ -0,0 +1,20 @@
+﻿using SharpCRUD.Testing.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpCRUD.Domain.Tests
+{
+    public class DomainBaseTest
+    {
+        protected IServiceProvider _serviceProvider;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp() 
+        {
+            _serviceProvider = SharpCRUDTesting.GetTestServiceProvider();
+        }
+    }
+}
---
+++ SharpCRUD.Domain.Tests/Services/CustomerModels/SaveCustomerServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SharpCRUD.Domain.Services.Shared;
+using SharpCRUD.Shared.CustomerModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpCRUD.Domain.Tests.Services.CustomerModels
+{
+    public class SaveCustomerServiceTests : DomainBaseTest
+    {
+        [Test]
+        public void CustomerServiceIsAvailable()
+        {
+            Assert.IsNotNull(
+                _serviceProvider.GetService<ISaveService<CustomerDto>>());
+        }
+
+        [Test]
+        public async Task SaveNullThrowsError()
+        {
+            var customerService = _serviceProvider.GetService<ISaveService<CustomerDto>>();
+
+            Assert.ThrowsAsync<ArgumentNullException>(async() =>
+            {
+                await customerService.Save(null);
+            });
+        }
+
+    }
+}
